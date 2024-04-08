c653eb293f22466c33b9de78ae29ee9aef737967
Roberto Quintero <langdonhp435@gmail.com>
2024-04-07 21:28:12 -0600

    Agrega formulario para editar growing_pigs

--- app/farms/custom/growing_pigs/components/GrowingPigsChangeStage.tsx
+++ app/farms/custom/growing_pigs/components/GrowingPigsChangeStage.tsx
@@ -4,23 +4,34 @@ import { UiContext } from '@/app/context/ui/UiContext'
 import { IGrowingPigs } from '@/interfaces/growing_pigs'
 import { MenuItem, TextField } from '@mui/material'
 import React, { SyntheticEvent, useContext, useState } from 'react'
+import { useForm } from 'react-hook-form'
 
 export const GrowingPigsChangeStage = () => {
   const {toggleModal} = useContext(UiContext)
-  const {growing_pig,pigStages,farmsLoading,postGrowingPigs} = useContext(FarmsContext)
-  const [stage, setStage] = useState(growing_pig?.id_pig_stage)
+  const {growing_pig,pigStages,farmsLoading,postGrowingPigs,ubications} = useContext(FarmsContext)
 
-  const onSubmit = async(e:SyntheticEvent) =>{
-    e.preventDefault()
-    if(growing_pig?.id_pig_stage===Number(stage)){
-      toggleModal()
-      return
-    }
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IGrowingPigs>()
+
+  const values={
+    ...growing_pig
+  } as IGrowingPigs
+
+  const onSubmit = async(data:IGrowingPigs) =>{
+    // e.preventDefault()
+    // if(growing_pig?.id_pig_stage===Number(stage)){
+    //   toggleModal()
+    //   return
+    // }
     const growing={
       ...growing_pig,
-      id_pig_stage:stage
+      ...data
      } as IGrowingPigs
-
+     console.log(growing)
+     return
      const ok= await postGrowingPigs(growing)
      if(ok){
       toggleModal()
@@ -28,14 +39,28 @@ export const GrowingPigsChangeStage = () => {
   };
 
   return (
-    <form className='Form' style={{width:'270px'}} onSubmit={onSubmit}>
+    <form className='Form' style={{width:'270px'}} onSubmit={handleSubmit(onSubmit)}>
+      <TextField 
+        size="small"
+        fullWidth
+        label='Cantidad'
+        type="number"
+        defaultValue={values.quantity}
+        {...register('quantity',{min:1})}
+        />
+      <TextField 
+        size="small"
+        fullWidth
+        label='Peso Promedio'
+        type="number"
+        defaultValue={values.average_weight}
+        {...register('average_weight',{min:1})}
+        />
       <TextField
           size="small"
           label='Etapa'
-          value={stage}
-          onChange={(e:React.ChangeEvent<HTMLInputElement>)=>{
-            setStage(+e.target.value)
-          }}
+          defaultValue={values.id_pig_stage}
+          {...register('id_pig_stage')}
           select >
           {
             pigStages.length
@@ -43,6 +68,24 @@ export const GrowingPigsChangeStage = () => {
               <MenuItem 
                 key={item.id_pig_stage} 
                 value={item.id_pig_stage}>
+                {item.description}
+              </MenuItem>
+            ))
+            :<div></div>
+          }
+        </TextField>
+      <TextField
+          size="small"
+          label='Ubicación'
+          defaultValue={values.id_ubication}
+          {...register('id_ubication')}
+          select >
+          {
+            ubications.filter(p=>p.id_pig_type===2).length
+            ?ubications.filter(p=>p.id_pig_type===2).map(item=>(
+              <MenuItem 
+                key={item.id_ubication} 
+                value={item.id_ubication}>
                 {item.description}
               </MenuItem>
             ))
